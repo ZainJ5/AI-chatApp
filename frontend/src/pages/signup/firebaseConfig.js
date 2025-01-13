98ca4c93376dd4bf69637035b2e6e2adbb9168b1
Zain Jamshaid <154011623+ZainJ5@users.noreply.github.com>
2025-01-13 23:36:41 +0500

    Update firebaseConfig.js

--- frontend/src/pages/signup/firebaseConfig.js
+++ frontend/src/pages/signup/firebaseConfig.js
@@ -3,19 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnMscEpjBugNdk99UdKgILjDNs_riNBEM",
-  authDomain: "aceinterview-4fbbb.firebaseapp.com",
-  projectId: "aceinterview-4fbbb",
-  storageBucket: "aceinterview-4fbbb.appspot.com",
-  messagingSenderId: "12462264746",
-  appId: "1:12462264746:web:481ecd96bd48c86e981bd3",
-  measurementId: "G-P41EBDK0TQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
